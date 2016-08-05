20ad6cd71adcc0c52ab639fc221769c1c594088a
Melissa L Kelley <mail@melissakelley.org>
2016-08-05 00:51:22 -0500

    Added one (failing) RSpec test as an example.

---
+++ spec/features/creating_location_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature 'Admins can create new locations' do
+  scenario 'with valid attributes' do
+    visit '/'
+    click_link 'New Location'
+    fill_in 'Name', with: 'Test Location'
+    click_button 'Create'
+    expect(page).to have_content 'Location has been created.'
+  end
+end
